ac43c334c50cf33075e962a1b4f9c1a138f50974
Elena Neroslavskaya <eneroslavskaya@pivotal.io>
2017-08-18 10:35:37 -0400

    add WF samples

---
+++ WFService-Calc/CodeActivity1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.IO;
+
+namespace CalculatorWFService
+{
+
+    public sealed class CodeActivity1 : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+        public OutArgument<string> result { get; set; }
+
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            string text = context.GetValue(this.Text);
+
+            Console.WriteLine($"Result is: {text}");
+
+            context.SetValue(result, "Result is:" + text);
+        }
+    }
+}
