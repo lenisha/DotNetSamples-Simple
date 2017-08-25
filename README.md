a760a020dee51fc7bd7604f8fa40e17118626f9a
Elena Neroslavskaya <eneroslavskaya@pivotal.io>
2017-08-24 20:05:56 -0400

    tcp service

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ Hosting simple WCF REST service using HWC and Binary buildpacks
 
 `/api/env` - print all environment variables
 
+## Self hosted TCP routing service
+
+https://docs.pivotal.io/pivotalcf/1-11/adminguide/enabling-tcp-routing.html
+cf create-shared-domain <space> tcp.<apps-domain>  --router-group default-tcp
+cf update-quota default —reserved-route-ports 100
+
+cf create-route pcfdev-space tcp.local.pcfdev.io --random-port
+cf map-route gs-spring tcp.local.pcfdev.io --port 61015
+
+
 # WF applications on PCF
 Windows Workflow Foundation has 3 models of hosting the flows
 https://docs.microsoft.com/en-us/dotnet/framework/windows-workflow-foundation/using-workflowinvoker-and-workflowapplication,
---
+++ WcfService-Tcp/service/service.cs
@@ -0,0 +1,91 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+using System;
+using System.Net;
+using System.ServiceModel;
+using System.Net.Sockets;
+using System.Linq;
+
+namespace Microsoft.Samples.NetTcp
+{
+    // Define a service contract.
+    [ServiceContract(Namespace="http://Microsoft.Samples.NetTcp")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+
+	// Service class which implements the service contract.
+	// Added code to write output to the console window
+	[ServiceBehavior(AddressFilterMode = AddressFilterMode.Any)]
+	public class CalculatorService : ICalculator
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        // Host the service within this EXE console application.
+        public static void Main()
+        {
+			// get PORT assigned to app from PCF env var
+			var port = Environment.GetEnvironmentVariable("PORT");
+			if (string.IsNullOrEmpty(port))
+			{
+				port = "9743";
+			}
+			var localip = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+			if (localip == null)
+			{
+				Console.Error.WriteLine("Couldn't find IP address of container");
+				Environment.Exit(1);
+			}
+			// Create a ServiceHost for the CalculatorService type.
+			using (ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService), new Uri($"net.tcp://{localip}:{port}/servicemodelsamples/service")))
+            {
+                // Open the ServiceHost to create listeners and start listening for messages.
+                serviceHost.Open();
+				Console.WriteLine($"Server has started on {localip}:{port}.\r\nWaiting for a connection...");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
